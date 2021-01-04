d39ebea6e99b57a94b00df26f8ca2bc4754a169e
jinkai <jinkaial1234@163.com>
2021-01-04 23:21:26 +0800

    'commit'

---
+++ app/src/main/java/com/jk/mymeet/fragment/chat/AllFriendFragment.java
@@ -0,0 +1,23 @@
+package com.jk.mymeet.fragment.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jk.framework.base.BaseFragment;
+import com.jk.mymeet.R;
+
+/**
+ * Created By Admin on 2021/1/4
+ * Describe:
+ */
+public class AllFriendFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat_record, null);
+//        initView(view);
+        return view;
+    }
+}
